518e7307c78ed6e38ae927c842640b784914d3d3
jc193322 <phillip.cluff@my.jcu.edu.au>
2019-08-28 13:13:21 +1000

    did all todo's in list_comprehensions

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -37,5 +37,7 @@ almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 numbers = [int(almost_number) for almost_number in almost_numbers]
 print(numbers)
 
-# TODO: use a list comprehension to create a list of only the numbers that are
+#  use a list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
+numbers_greater_than_nine = [number for number in numbers if number >= 9]
+print(numbers_greater_than_nine)
