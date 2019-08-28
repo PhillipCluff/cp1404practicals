e96bdf66bc025bcc28fac155a3c377276b6bc4f4
jc193322 <phillip.cluff@my.jcu.edu.au>
2019-08-28 12:53:15 +1000

    fix password_checker

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -4,11 +4,16 @@
 def main():
 
     min_length = 5
+    password = get_password(min_length)
+    for i in range(0, len(password)):
+        print("*", end='')
+
+
+def get_password(min_length):
     password = input("Please enter password: ")
     while len(password) < min_length:
         password = input("Password not long enough: ")
-    for i in range(0, len(password)):
-        print("*", end='')
+    return password
 
 
 main()
