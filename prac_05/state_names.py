479befcd856e88e76f353f07d27d8ac49b451bab
jc193322 <phillip.cluff@my.jcu.edu.au>
2019-09-11 11:47:42 +1000

    Fix code from review

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -12,7 +12,7 @@ STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Te
 state = input("Enter short state: ").upper()
 while state != "":
     if state in STATE_NAMES:
-        print(state, "is", STATE_NAMES[state])
+        print("{} is {}".format(state, STATE_NAMES[state]))
     else:
         print("Invalid short state")
     state = input("Enter short state: ").upper()
