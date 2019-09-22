1515daa22364ead1c644f71e1a73ba5ff82e75b2
PhillipCluff <phillip.cluff@my.jcu.edu.au>
2019-09-22 22:51:15 +1000

    Fix error in guitars program

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -3,7 +3,13 @@ from prac_06.guitar import Guitar
 
 def main():
     guitars = []
-    fender = Guitar("Fender Stratocaster", 2014, 765.40))
+    fender = Guitar("Fender Stratocaster", 2014, 765.40)
     guitars.append(fender)
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    for guitar in guitars:
+        print(guitar)
+
+
+main()
