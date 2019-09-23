bb4297f1717bea8bd8e635f11ff2c1273505ad4c
PhillipCluff <phillip.cluff@my.jcu.edu.au>
2019-09-23 19:15:16 +1000

    Add incomplete print string

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -8,8 +8,16 @@ def main():
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
-    for guitar in guitars:
-        print(guitar)
+    for i, guitar in enumerate(guitars, 1):
+        if guitar.is_vintage():
+            vintage_string = " (vintage)"
+        else:
+            vintage_string = ""
+        print("Guitar {}: {:>20} ({}), worth ${:10,.2f}{}".format(i,
+                                                                  guitar.name,
+                                                                  guitar.year,
+                                                                  guitar.cost,
+                                                                  vintage_string))
 
 
 main()
