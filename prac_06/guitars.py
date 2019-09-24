141f8f29abbb3a398fced5958076eef7cab30627
PhillipCluff <phillip.cluff@my.jcu.edu.au>
2019-09-24 21:49:37 +1000

    Add user input Change print part

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -3,21 +3,33 @@ from prac_06.guitar import Guitar
 
 def main():
     guitars = []
-    fender = Guitar("Fender Stratocaster", 2014, 765.40)
-    guitars.append(fender)
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(guitar_to_add, "added.")
+        name = input("Name: ")
+
+    # fender = Guitar("Fender Stratocaster", 2014, 765.40)
+    # guitars.append(fender)
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
-    for i, guitar in enumerate(guitars, 1):
-        if guitar.is_vintage():
-            vintage_string = " (vintage)"
-        else:
-            vintage_string = ""
-        print("Guitar {}: {:>20} ({}), worth ${:10,.2f}{}".format(i,
-                                                                  guitar.name,
-                                                                  guitar.year,
-                                                                  guitar.cost,
-                                                                  vintage_string))
+    if guitars:
+        print("These are my guitars:")
+        guitars.sort()
+
+        for i, guitar in enumerate(guitars, 1):
+            if guitar.is_vintage():
+                vintage_string = " (vintage)"
+            else:
+                vintage_string = ""
+            print("Guitar {0}: {1.name:>20} ({1.year}), worth ${1.cost:10,.2f}{2}".format(i,
+                                                                                          guitar,
+                                                                                          vintage_string))
 
 
 main()
