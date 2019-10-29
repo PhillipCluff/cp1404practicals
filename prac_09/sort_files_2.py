1a227a7ae170da0d14207026381c11491c30eccc
PhillipCluff <phillip.cluff@my.jcu.edu.au>
2019-10-29 22:56:25 +1000

    End work day

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -7,8 +7,8 @@ extensions_to_category = {}
 
 for filename in os.listdir('.'):
     if filename.split('.')[-1] not in extensions_to_category:
-        extensions_to_category[filename.split('.')[-1]] = filename.split('.')[-1]
+        extensions_to_category[filename.split('.')[-1]] = 1
     else:
-        pass
+        extensions_to_category[filename.split('.')[-1]] += 1
 
 print(extensions_to_category)
